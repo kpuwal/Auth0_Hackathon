de265f57e9a313899dab5e7f8959c1e422faa0fb
kpuwal <kpuwal@users.noreply.github.com>
2021-08-03 16:16:40 +0100

    adds heroku remote and postgresql addon

---
+++ server/db/config.ts
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const pgp = require('pg-promise')();
+const isProduction = process.env.NODE_ENV === 'production';
+
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+const db = pgp({
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  ssl: isProduction,
+  capSQL: true
+});
+
+module.exports = {
+  pgp, db
+};
