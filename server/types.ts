2d239ac37d307cd4e53030ffea897bdd0d847df6
kpuwal <kpuwal@users.noreply.github.com>
2021-08-12 22:03:33 +0100

    adds types to sentiment

---
+++ server/types.ts
@@ -0,0 +1,86 @@
+type ApiNewsResponseStatus = 'ok' | 'error';
+
+type ApiNewsCountry = 'ae' | 'ar' | 'at' | 'au' | 'be' | 'bg' | 'br' | 'ca' | 'ch' | 'cn' | 'co' | 'cu' | 'cz' | 'de' | 'eg' | 'fr' | 'gb' | 'gr' | 'hk' | 'hu' | 'id' | 'ie' | 'il' | 'in' | 'it' | 'jp' | 'kr' | 'lt' | 'lv' | 'ma' | 'mx' | 'my' | 'ng' | 'nl' | 'no' | 'nz' | 'ph' | 'pl' | 'pt' | 'ro' | 'rs' | 'ru' | 'sa' | 'se' | 'sg' | 'si' | 'sk' | 'th' | 'tr' | 'tw' | 'ua' | 'us' | 've' | 'za';
+
+type ApiNewsLanguage = 'ar' | 'de' | 'en' | 'es' | 'fr' | 'he' | 'it' | 'nl' | 'no' | 'pt' | 'ru' | 'se' | 'ud' | 'zh';
+
+type ApiNewsCategory = 'business' | 'entertainment' | 'general' | 'health' | 'science' | 'sports' | 'technology';
+
+interface INewsApiSource {
+	id: string | null;
+	name: string;
+}
+
+export interface INewsApiSourceItem {
+	id: string;
+	name: string;
+	description: string;
+	url: string;
+	category: ApiNewsCategory;
+	language: ApiNewsLanguage;
+	country: ApiNewsCountry;
+}
+
+export interface INewsApiArticle {
+	source: INewsApiSource;
+	author: string | null;
+	title: string;
+	description: string | null;
+	url: string;
+	urlToImage: string | null;
+	publishedAt: string;
+	content: string | null;
+  analysed: {
+    score: number;
+    comparative: number;
+    calculation: [];
+    tokens: string[];
+    words: string[];
+    positive: string[];
+    negative: string[];
+  };
+}
+
+export interface INewsApiSourcesResponse {
+	status: ApiNewsResponseStatus;
+	code?: string;
+	error?: string;
+	sources: Array<INewsApiSourceItem>;
+}
+
+export interface INewsApiHeadlinesResponse {
+  status: ApiNewsResponseStatus;
+  totalResults: number;
+  articles: INewsApiArticle[];
+  error?: string;
+  code?: string;
+}
+
+export interface ISources {
+  country: string
+}
+
+export interface IHeadlines {
+  title: string
+}
+
+export interface ISentimentAnalysis {
+  positive: INewsApiArticle[];
+  neutral: INewsApiArticle[];
+  negative: INewsApiArticle[];
+}
+
+type sourcesType = {
+  country: string,
+  apiKey: string | undefined
+}
+
+type headlinesType = {
+  sources: string,
+  sortBy: string,
+  apiKey: string | undefined
+}
+
+export type axiosRequestType = INewsApiSourcesResponse | INewsApiHeadlinesResponse;
+
+export type configType = sourcesType | headlinesType;
