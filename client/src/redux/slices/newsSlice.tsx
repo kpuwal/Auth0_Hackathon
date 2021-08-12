34eb4925ccc3024fb60cc4dd1d32c1bc0bcf3fb5
kpuwal <kpuwal@users.noreply.github.com>
2021-08-12 22:56:42 +0100

    adds newsSlice to redux store

---
+++ client/src/redux/slices/newsSlice.tsx
@@ -0,0 +1,55 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { IData, newsInitStateType } from '../types'; 
+
+const initialState: newsInitStateType = {
+  titles: [],
+  headlines: {
+    positive: [],
+    neutral: [],
+    negative: [],
+  },
+  loading: 'idle'
+}
+
+const fetchData = async (url: string, data: string, country?: string) => {
+  const config = {
+    method: 'POST',
+    body: JSON.stringify({data, country}),
+    headers: { 'Content-Type': 'application/json' }
+  } 
+  
+  try {
+    const newsData = await fetch(url, config);
+    return newsData.json();
+  } catch(err) { return err };
+}
+
+export const fetchTitles = createAsyncThunk(
+  'sources',
+  async (country: string) => {
+    return fetchData('/sources', country);
+  }
+)
+
+export const fetchHeadlines = createAsyncThunk(
+  'authHeadlines',
+  async (data: IData) => {
+    return fetchData('/headlines', data.title, data.country);
+  }
+)
+
+export const newsSlice = createSlice({
+  name: 'news',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchTitles.fulfilled, (state, action) => {
+      state.titles = action.payload
+    })
+    builder.addCase(fetchHeadlines.fulfilled, (state, action) => {
+      state.headlines = action.payload
+    })
+  },
+})
+
+export default newsSlice.reducer;
