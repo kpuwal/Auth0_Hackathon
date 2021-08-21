c4b107fb6ed20ea26b6d43eddeaaa0f09651ef68
kpuwal <kpuwal@users.noreply.github.com>
2021-08-21 01:51:13 +0100

    adds svg mood icons

--- client/src/components/stats/BarGroup.tsx
+++ client/src/components/stats/BarGroup.tsx
@@ -3,7 +3,7 @@ import { countryProp } from '../../redux/types';
 const BarGroup = ({ moods, country, sum }: countryProp) => {
   return (
     <>
-    <svg width={250} height={200} viewBox="0 0 100 160">
+    <svg width={250} height={200} viewBox={`0 0 200 160`} style={{ }}>
       <g transform="matrix(1 0 0 -1 100 100)">
         <rect x={-70} y={0} width={20} height={90} fill="#f1f1f1" rx="3" ry="3" />
         <rect x={-45} y={0} width={20} height={90} fill="#f1f1f1" rx="3" ry="3" />
@@ -25,19 +25,25 @@ const BarGroup = ({ moods, country, sum }: countryProp) => {
         <text className="name-label" x="0px" y="10px" alignmentBaseline="middle" >{country}</text>
       </g>
       <g>
-        <text className="name-label" x="0px" y="130px" alignmentBaseline="middle">number of headlines: </text>
+        <text className="name-label" x="0px" y="130px" alignmentBaseline="middle">number of headlines analysed: </text>
       </g>
       <g>
-        <text className="name-label-bold" x="100px" y="130px" alignmentBaseline="middle">{ }{sum}</text>
+        <text className="name-label-bold" x="100px" y="130px" alignmentBaseline="middle">{" "}{sum}</text>
       </g>
-    </svg>
-    <svg 
-      className="emoji-label" 
-      height="20px"
-      width="20px"
-      viewBox="0 0 24 24" 
-      fill="#000000">
-      <g>
+      <g transform="translate(32, 103) scale(0.6 0.6)">
+        <path d="M0 0h24v24H0V0z" fill="none"/>
+        <circle cx="15.5" cy="9.5" r="1.5"/>
+        <circle cx="8.5" cy="9.5" r="1.5"/>
+        <path d="M12 16c-1.48 0-2.75-.81-3.45-2H6.88c.8 2.05 2.79 3.5 5.12 3.5s4.32-1.45 5.12-3.5h-1.67c-.69 1.19-1.97 2-3.45 2zm-.01-14C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>
+      </g>
+      <g transform="translate(58, 103) scale(0.6 0.6)">
+        <path d="M0 0h24v24H0V0z" fill="none"/>
+        <path d="M9 14h6v1.5H9z"/>
+        <circle cx="15.5" cy="9.5" r="1.5"/>
+        <circle cx="8.5" cy="9.5" r="1.5"/>
+        <path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>
+      </g>
+      <g transform="translate(83, 103) scale(0.6 0.6)">
         <path d="M0 0h24v24H0V0z" fill="none"/>
         <circle cx="15.5" cy="9.5" r="1.5"/>
         <circle cx="8.5" cy="9.5" r="1.5"/>
