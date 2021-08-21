67c9b9d4d5fc470495340b536bd9463c6c5d36c6
kpuwal <kpuwal@users.noreply.github.com>
2021-08-21 22:34:09 +0100

    adds style to Statistics

--- server/services/prepStats.ts
+++ server/services/prepStats.ts
@@ -15,40 +15,6 @@ const convertToPercent = (data: dataProps[], sum: number) => {
   })
 }
 
-export const prepStatsCountries = (data: Icountries[]) => {
-  const uniqueCountries = [...new Set(data.map((a) => a.country))];
-  const flattened = uniqueCountries.map(country => {
-    const flatMood = data
-      .filter(item => item.country.match(country))
-      .map(a => {
-        return {mood: a.mood, count: parseInt(a.count)}
-      })
-
-    const sum = findSum(flatMood);
-    const percentage = convertToPercent(flatMood, sum);
-    const newObj = Object.assign(percentage[0], percentage[1], percentage[2]);
-    return {country, moods: newObj, sum};
-  })
-  return flattened;
-}
-
-export const prepStatsTitles = (data: Ititles[]) => {
-  const uniqueCountries = [...new Set(data.map((a) => a.source))];
-  const flattened = uniqueCountries.map(source => {
-    const flatMood = data
-      .filter(item => item.source === source)
-      .map(a => {
-        return {mood: a.mood, count: parseInt(a.count)}
-      })
-    
-    const sum = findSum(flatMood);
-    const percentage = convertToPercent(flatMood, sum);
-    const newObj = Object.assign(percentage[0], percentage[1], percentage[2]);
-    return {source, moods: newObj, sum};
-  })
-  return flattened;
-}
-
 const convertDate = (date: string | number) => {
   const mydate = new Date(date);
   const month =  mydate.getMonth();
@@ -83,6 +49,40 @@ const filterMoods = (data: any) => {
     })
   }
   return filtered;
+}
+
+export const prepStatsCountries = (data: Icountries[]) => {
+  const uniqueCountries = [...new Set(data.map((a) => a.country))];
+  const flattened = uniqueCountries.map(country => {
+    const flatMood = data
+      .filter(item => item.country.match(country))
+      .map(a => {
+        return {mood: a.mood, count: parseInt(a.count)}
+      })
+
+    const sum = findSum(flatMood);
+    const percentage = convertToPercent(flatMood, sum);
+    const newObj = Object.assign(percentage[0], percentage[1], percentage[2]);
+    return {country, moods: newObj, sum};
+  })
+  return flattened;
+}
+
+export const prepStatsTitles = (data: Ititles[]) => {
+  const uniqueCountries = [...new Set(data.map((a) => a.source))];
+  const flattened = uniqueCountries.map(source => {
+    const flatMood = data
+      .filter(item => item.source === source)
+      .map(a => {
+        return {mood: a.mood, count: parseInt(a.count)}
+      })
+    
+    const sum = findSum(flatMood);
+    const percentage = convertToPercent(flatMood, sum);
+    const newObj = Object.assign(percentage[0], percentage[1], percentage[2]);
+    return {source, moods: newObj, sum};
+  })
+  return flattened;
 }
 
 export const prepStatsDates = (data: Idates[]) => {
