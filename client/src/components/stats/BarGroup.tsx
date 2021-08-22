22473ba7250254ae91732fb63fb6ebc931aa9cbd
kpuwal <kpuwal@users.noreply.github.com>
2021-08-22 17:59:13 +0100

    adds WorldGraph scale, refactors icon into separate file

--- client/src/components/stats/BarGroup.tsx
+++ client/src/components/stats/BarGroup.tsx
@@ -1,4 +1,5 @@
 import { countryProp } from '../../redux/types';
+import { HappyIcn, NeutralIcn, NegativeIcn } from './Icons';
 
 const BarGroup = ({ moods, country, sum }: countryProp) => {
   return (
@@ -30,22 +31,9 @@ const BarGroup = ({ moods, country, sum }: countryProp) => {
       <g>
         <text className="name-label-bold" x="100px" y="130px" alignmentBaseline="middle">{" "}{sum}</text>
       </g>
-      <g transform="translate(32, 103) scale(0.6 0.6)">
-        <path d="M0 0h24v24H0V0z" fill="none"/>
-        <circle cx="15.5" cy="9.5" r="1.5"/>
-        <circle cx="8.5" cy="9.5" r="1.5"/>
-        <path d="M12 16c-1.48 0-2.75-.81-3.45-2H6.88c.8 2.05 2.79 3.5 5.12 3.5s4.32-1.45 5.12-3.5h-1.67c-.69 1.19-1.97 2-3.45 2zm-.01-14C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>
-      </g>
-      <g transform="translate(58, 103) scale(0.6 0.6)">
-        <path d="M0 0h24v24H0z" fill="none"/>
-        <path d="M9 11.75c-.69 0-1.25.56-1.25 1.25s.56 1.25 1.25 1.25 1.25-.56 1.25-1.25-.56-1.25-1.25-1.25zm6 0c-.69 0-1.25.56-1.25 1.25s.56 1.25 1.25 1.25 1.25-.56 1.25-1.25-.56-1.25-1.25-1.25zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8 0-.29.02-.58.05-.86 2.36-1.05 4.23-2.98 5.21-5.37C11.07 8.33 14.05 10 17.42 10c.78 0 1.53-.09 2.25-.26.21.71.33 1.47.33 2.26 0 4.41-3.59 8-8 8z"/>
-      </g>
-      <g transform="translate(83, 103) scale(0.6 0.6)">
-        <path d="M0 0h24v24H0V0z" fill="none"/>
-        <circle cx="15.5" cy="9.5" r="1.5"/>
-        <circle cx="8.5" cy="9.5" r="1.5"/>
-        <path d="M12 14c-2.33 0-4.32 1.45-5.12 3.5h1.67c.69-1.19 1.97-2 3.45-2s2.75.81 3.45 2h1.67c-.8-2.05-2.79-3.5-5.12-3.5zm-.01-12C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/>
-      </g>
+      <HappyIcn scale={0.6} posX={32} posY={103} />
+      <NeutralIcn scale={0.6} posX={58} posY={103} />
+      <NegativeIcn scale={0.6} posX={83} posY={103} />
     </svg>
   </>
   )
--- client/src/components/stats/WorldGraph.tsx
+++ client/src/components/stats/WorldGraph.tsx
@@ -1,6 +1,7 @@
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { prepData } from './helper';
+import { HappyIcn } from './Icons';
 
 const MONTHS: string[] = [
   "Jan",
@@ -17,27 +18,41 @@ const MONTHS: string[] = [
   "Dec",
 ];
 
+const positionX = 80;
+
 const WorldGraph = () => {
   const dates = useSelector((state: RootState) => state.stats.byDates);
   const data = prepData(dates);
   console.log(data)
 
   return (
-    <svg width="100%" height="200px" viewBox={`0 0 900 350`}>
-      <g transform="matrix(1 0 0 -1 0 290)">
+    <svg width="100%" height="200px" viewBox={`0 0 900 400`}>
+      <g>
+        {
+          MONTHS.map((item, idx) => 
+          <g key={idx}>
+            <line x1={positionX * idx} y1={30} x2={positionX * idx} y2={370} style={{stroke: "#e9e5dd", opacity: .4, strokeWidth: "2px"}} />
+            <line x1={positionX * idx} y1={360} x2={positionX * idx} y2={370} style={{stroke: "#1f1f1f", strokeWidth: "2px"}} />
+          </g>)
+        }
+        <line x1={-20} y1={360} x2={900} y2={360} style={{stroke: "#1f1f1f", strokeWidth: "2px"}} />
+        <line x1={0} y1={30} x2={0} y2={370} style={{stroke: "#1f1f1f", strokeWidth: "1px"}} />
+      </g>
+      <g transform="matrix(1 0 0 -1 0 340)">
         <polyline points={data.posPoints} style={{fill: "none", opacity: .2, stroke: "#1f1f1f", strokeWidth: 5}} />
       </g>
-      <g transform="matrix(1 0 0 -1 0 295)">
+      <g transform="matrix(1 0 0 -1 0 345)">
         <polyline points={data.neuPoints} style={{fill: "none", opacity: 1, stroke: "#1f1f1f", strokeWidth: 5}} />
       </g>
-      <g transform="matrix(1 0 0 -1 0 300)">
+      <g transform="matrix(1 0 0 -1 0 350)">
         <polyline points={data.negPoints} style={{fill: "none", opacity: .4, stroke: "#1f1f1f", strokeWidth: 5}} />
       </g>
       <g>
         {
-          MONTHS.map((item, idx) => <text x={80 * idx} y={330} alignmentBaseline="middle" >{item}</text>)
+          MONTHS.map((item, idx) => <text key={idx} x={positionX * idx} y={390} alignmentBaseline="middle" >{item}</text>)
         }
       </g>
+      <HappyIcn scale={1.6} posX={88} posY={109} />
     </svg>
   )
 }
