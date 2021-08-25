eb92fde3be4b9c71c593651e679cac06fc202ba5
kpuwal <kpuwal@users.noreply.github.com>
2021-08-25 10:10:31 +0100

    fix style of bar chart

--- client/src/components/stats/BarGroup.tsx
+++ client/src/components/stats/BarGroup.tsx
@@ -17,7 +17,7 @@ interface IbarGroup {
 
 const BarGroup = ({ moods, label, sum, dominant }: IbarGroup) => {
   return (
-    <svg width={300} height={210} viewBox={`0 0 200 170`}>
+    <svg width={280} height={210} viewBox={`0 0 200 170`}>
       {/* positive */}
       <Bar id="positive" mood={moods.positive} barX={-70} valX={131} dominant={dominant.mood} />
       <HappyIcn scale={0.6} posX={132} posY={113} opacity={dominant.mood === "positive" ? 1 : .4} />
@@ -32,13 +32,17 @@ const BarGroup = ({ moods, label, sum, dominant }: IbarGroup) => {
 
       <g>
         <text className="name-label-bold" x={posX} y={5}alignmentBaseline="middle">{label}</text>
+
+        <line x1={posX - 8} y1={13} x2={250} y2={13} style={{stroke: "#1f1f1f", strokeWidth: .5}} opacity={.2} />
+        <circle cx={211} cy={13} r={1.5} stroke="#a9a9a9" strokeWidth={.5} fill="#ffffff" />
+
         <text className="name-label" x={posX} y={posY} alignmentBaseline="middle">number of headlines </text>
 
         <text className="name-label" x={posX} y={posY + 10}alignmentBaseline="middle">analysed: </text>
         <text className="name-label-bold" x={posX + 38} y={posY + 12} alignmentBaseline="middle">{sum}</text>
 
         <text className="name-label" x={posX} y={posY + 40} alignmentBaseline="middle">dominant mood:</text>
-        <text className="name-label-bold" x={posX} y={posY + 55} alignmentBaseline="middle">{dominant.mood}</text>
+        <text className="name-label-bold-big" x={posX} y={posY + 55} alignmentBaseline="middle">{dominant.mood}</text>
       </g>
     </svg>
   )
