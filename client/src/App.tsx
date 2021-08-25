144a8d8f431222c7e677e6cc433b4c0265f47149
kpuwal <kpuwal@users.noreply.github.com>
2021-08-25 21:20:15 +0100

    adds style to stats side menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Main from './components/Main';
 import Statistics from './components/Statistics';
 import Profile from './components/Profile';
 import About from './components/About';
+
 import ProtectedRoute from './components/auth/ProtectedRoute';
 import { Route, Switch } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
@@ -19,9 +20,9 @@ const App = () => {
       <Navigation />
         <Switch>
           <Route exact path="/" component={Main} />
+          <Route path="/about" component={About} />
           <ProtectedRoute path="/statistics" component={Statistics} />
           <ProtectedRoute path="/profile" component={Profile} />
-          <Route path="/about" component={About} />
           <Route render={() => <h1>404: page not found</h1>} />
         </Switch>
     </>
--- client/src/components/Statistics.tsx
+++ client/src/components/Statistics.tsx
@@ -10,28 +10,44 @@ const Statistics = () => {
   useFetchStats();
   const countries = useSelector((state: RootState) => state.stats.byCountries);
   const titles = useSelector((state: RootState) => state.stats.byTitles);
-
+  console.log("height: ", window.innerHeight)
   return (
     <div className="container">
       <div className="row justify-content-center">
+
         <div className="col-4 info-container fixed-top">
-          <h4>filter by country mood:</h4>
-          <h4>Newspaper title mood:</h4>
+          <div className="headerLabel">Filter:</div>
+          <ul className="sidebar-menu">
+            <li><a className="sidebar-menu-item" href='#statsworld'>stats for the world {">"}</a></li>
+            <li><a className="sidebar-menu-item" href='#statscountries'>stats for the countries {">"}</a></li>
+            <li><a className="sidebar-menu-item" href='#statstitles'>stats for the titles {">"}</a></li>
+          </ul>
         </div>
+
         <div className="col-8 charts-container">
-          <div  className="chart-box">
+          <div 
+            className="chart-box"
+            id="statsworld"
+            style={{height: window.innerHeight, paddingTop: "80px"}}>
             <WorldGraph />
           </div>
-          <div className="chart-box">
+          <div 
+            className="chart-box" 
+            id="statscountries" 
+            style={{height: window.innerHeight, paddingTop: "155px"}}>
             {
               countries.map((country, idx) => <BarGroup key={idx} moods={country.moods} label={findCountryLabel(country.country)} sum={country.sum} dominant={country.dominant} />)
             }
           </div>
-          <div className="chart-box"></div>
+          <div
+            className="chart-box" 
+            id="statstitles"
+            style={{paddingTop: "100px"}}>
             {
               titles.map((title, idx) => <BarGroup key={idx} moods={title.moods} label={title.source} sum={title.sum} dominant={title.dominant} />)
             }
           </div>
+        </div>
       </div>
     </div>
   );
