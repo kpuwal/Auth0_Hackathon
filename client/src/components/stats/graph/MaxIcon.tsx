c51d2b125970eb0d5e6c28d8998a82b4ef174f9a
kpuwal <kpuwal@users.noreply.github.com>
2021-08-26 01:56:44 +0100

    refactors MaxIcon

--- client/src/components/stats/graph/MaxIcon.tsx
+++ client/src/components/stats/graph/MaxIcon.tsx
@@ -1,7 +1,6 @@
+import MaxIconItem from './MaxIconItem';
 import { HappyIcn, NeutralIcn, NegativeIcn, SpeechBubble } from '../Icons';
 
-const rectBg: string = "#ffffff";
-const lineStroke: string = "#a9a9a9";
 const iconSize: number = 1.6;
 
 export interface ImaxIcon {
@@ -16,45 +15,24 @@ const MaxIcon = ({name, posX, posY, txt, maxStyle}: ImaxIcon) => {
     switch (name) {
       case "positive":
         return (
-          <svg className={maxStyle}>
-            <g>
-              <rect x={posX} y={0} width={40} height={350} fill={rectBg} />
-              <line x1={posX + 19} y1={posY} x2={posX+ 19} y2={360} style={{stroke: `${lineStroke}`, strokeWidth: "2px"}} />
-
-              <HappyIcn scale={iconSize} {...{posX, posY}} />
-              <SpeechBubble scale={iconSize} {...{posX, posY}} />
-              
-              <text className="speech-bubble-graph-txt" x={posX + 38} y={posY - 10}>{txt}</text>
-            </g>
-          </svg>
+          <MaxIconItem {...{posX, posY, txt, maxStyle}}>
+            <HappyIcn scale={iconSize} {...{posX, posY}} />
+            <SpeechBubble scale={iconSize} {...{posX, posY}} />
+          </MaxIconItem>
         )
       case "neutral":
         return (
-          <svg className={maxStyle}>
-            <g>
-              <rect x={posX} y={0} width={40} height={350} fill={rectBg} />
-              <line x1={posX + 19} y1={posY} x2={posX+ 19} y2={360} style={{stroke: `${lineStroke}`, strokeWidth: "2px"}} />
-
-              <NeutralIcn scale={iconSize} {...{posX, posY}} />
-              <SpeechBubble scale={iconSize} {...{posX, posY}} />
-
-              <text className="speech-bubble-graph-txt" x={posX + 38} y={posY - 10}>{txt}</text>
-            </g>
-          </svg>
+          <MaxIconItem {...{posX, posY, txt, maxStyle}}>
+            <NeutralIcn scale={iconSize} {...{posX, posY}} />
+            <SpeechBubble scale={iconSize} {...{posX, posY}} />
+          </MaxIconItem>
         )
       case "negative":
         return (
-          <svg className={maxStyle}>
-            <g>
-              <rect x={posX} y={0} width={40} height={350} fill={rectBg} />
-              <line x1={posX + 19} y1={posY} x2={posX+ 19} y2={360} style={{stroke: `${lineStroke}`, strokeWidth: "2px"}} />
-
-              <NegativeIcn scale={iconSize} {...{posX, posY}} />
-              <SpeechBubble scale={iconSize} {...{posX, posY}} />
-              
-              <text className="speech-bubble-graph-txt" x={posX + 38} y={posY - 10}>{txt}</text>
-            </g>
-          </svg>
+          <MaxIconItem {...{posX, posY, txt, maxStyle}}>
+            <NegativeIcn scale={iconSize} {...{posX, posY}} />
+            <SpeechBubble scale={iconSize} {...{posX, posY}} />
+          </MaxIconItem>
         )
       default:
         return <g></g>
---
+++ client/src/components/stats/graph/MaxIconItem.tsx
@@ -0,0 +1,25 @@
+const rectBg: string = "#ffffff";
+const lineStroke: string = "#a9a9a9";
+
+interface ImaxIconItem {
+  children: React.ReactNode;
+  maxStyle?: string;
+  posX: number;
+  posY: number;
+  txt: number;
+}
+
+const MaxIconItem = ({children, posX, posY, txt, maxStyle}: ImaxIconItem) => {
+  return (
+    <svg className={maxStyle}>
+      <g>
+        <rect x={posX} y={0} width={40} height={350} fill={rectBg} />
+        <line x1={posX + 19} y1={posY} x2={posX+ 19} y2={360} style={{stroke: `${lineStroke}`, strokeWidth: "2px"}} />
+          {children}
+        <text className="speech-bubble-graph-txt" x={posX + 38} y={posY - 10}>{txt}</text>
+      </g>
+    </svg>
+  )
+}
+
+export default MaxIconItem;
