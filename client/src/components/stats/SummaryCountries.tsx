8edb5ce65219e59c581d91a90cef440c8993d906
kpuwal <kpuwal@users.noreply.github.com>
2021-08-28 13:10:52 +0100

    adds emoji to summary box

--- client/src/components/stats/SummaryCountries.tsx
+++ client/src/components/stats/SummaryCountries.tsx
@@ -1,3 +1,5 @@
+import { HappyIcn, NegativeIcn } from "./Icons"
+
 interface IsummaryCountry {
   happy: string,
   sad: string,
@@ -10,12 +12,20 @@ const SummaryCountries = ({happy, sad}: IsummaryCountry) => {
       <ul>
         <li>
           <div className="summary-box">
-            the <span>happiest</span> news comes from: <br/><span id="important">{happy}</span>
+            <svg width={14} height={14}>
+              <HappyIcn scale={.6} posX={0} posY={-1} />
+            </svg>
+            the <span>happiest</span> news comes from: <br/>
+            <span id="important">{happy}</span>
           </div>
         </li>
         <li>
-        <div className="summary-box second">
-            the <span>saddest</span> news comes from: <br/><span id="important">{sad}</span>
+        <div className="summary-box">
+            <svg width={15} height={15}>
+              <NegativeIcn scale={.6} posX={0} posY={-1} />
+            </svg>
+            the <span>saddest</span> news comes from: <br/>          
+            <span id="important">{sad}</span>
           </div>
         </li>
       </ul>
