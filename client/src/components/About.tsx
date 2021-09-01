3c32cbd11c1845122bb509a68f7c66234b24ecc3
kpuwal <kpuwal@users.noreply.github.com>
2021-09-01 01:13:12 +0100

    adds about and profile views

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -1,6 +1,18 @@
 const About = () => {
   return (
-    <div>this is about the app page</div>
+    <div className="container about-container">
+      <div className="row justify-content-md-center">
+        <div className="col col-lg-2" />
+        <div className="col-md-8">
+          <h1 style={{paddingBottom: "10vh", paddingTop: "5vh"}}>Hello 👀👋</h1>
+          <p>Have you been feeling lately that reading news every morning or even scanning headlines becomes more and more depressing?</p>
+          <p>What if you could skip the bad news and read the good ones only?</p>
+
+          <p>Grade_News! app to the rescue. My app is a news reader that classifies information into mood graded categories, allows you decide which ones you want to see and which ones are more likely  make you feel miserable.</p>
+        </div>
+        <div className="col col-lg-2" />
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Profile.tsx
+++ client/src/components/Profile.tsx
@@ -1,30 +1,43 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const user = useAuth0();
 
   return (
-    <div>
-      <div className="row align-items-center profile-header">
-        <div className="col-md-2 mb-3">
-          <img
-            src={user.user?.picture}
-            alt="Profile"
-            className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-          />
+        <div className="container about-container">
+          <div className="row justify-content-md-center">
+            <div className="col col-lg-2" />
+            <div className="col-md-8">
+              <h1 style={{paddingBottom: "10vh", paddingTop: "5vh"}}>Hello 👀👋 <br/>{user.user?.name}</h1>
+              <ButtonMailto
+                mailto={"mailto:kpuwal@gmail.com"}
+                label={"e-mail me to permanantly remove your account"}
+              />
+            </div>
+            <div className="col col-lg-2" />
+          </div>
         </div>
-        <div className="col-md text-center text-md-left">
-          <h2>{user.user?.name}</h2>
-          <p className="lead text-muted">{user.user?.email}</p>
-        </div>
-      </div>
-      <div className="row">
-        <pre className="col-12 text-light bg-dark p-4">
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      </div>
-    </div>
   );
 };
+
+interface IbuttonMailTo {
+  mailto: any,
+  label: string,
+}
+
+const ButtonMailto = ({ mailto, label }:IbuttonMailTo) => {
+    return (
+      <Link
+        to='#'
+        onClick={(e) => {
+          window.location = mailto;
+          e.preventDefault();
+        }}
+      >
+        {label}
+      </Link>
+    )
+}
 
 export default Profile;
